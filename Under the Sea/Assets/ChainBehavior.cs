c35e7b99e7d572030b304c373207fdb418b85c9c
connmatt <matt.connolly1225@gmail.com>
2018-11-02 21:59:20 -0700

    mine enemy created and follows player when player gets close enough to mine

---
+++ Under the Sea/Assets/ChainBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChainBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<CharacterJoint>().connectedBody = transform.parent.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Under the Sea/Assets/MineBehavior.cs
@@ -0,0 +1,41 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineBehavior : MonoBehaviour {
+
+	public GameObject Player;
+	private bool entered;
+	public float speed;
+	
+	public float step = 0.01f;
+	Vector3 del;
+	float dist;
+
+	Vector3 lookAt;
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	private void OnTriggerEnter(Collider col)
+	{
+		if(col.gameObject.tag == "Player")
+		{
+			entered = true;
+		}
+		else{
+			entered = false;
+		}
+
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(entered)
+		{
+			step = speed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, step);
+		}
+	}
+
+}
