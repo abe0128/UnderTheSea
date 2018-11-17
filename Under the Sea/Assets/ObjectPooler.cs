2e7a5bfdcdf7704249104250e7e6a391b07756ac
abe <adealba@csumb.edu>
2018-11-17 00:25:21 -0800

    my attempt at object pool

---
+++ Under the Sea/Assets/ObjectPooler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+    // Use this for initialization
+    void Start ()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+	}
+	
+}
