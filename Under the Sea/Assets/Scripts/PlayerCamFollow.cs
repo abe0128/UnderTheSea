8f29c64f7e8fc3faf9f514e50db89c4d046a1781
eric tinoco <ertinoco@csumb.edu>
2018-11-20 20:08:47 -0800

    Otter now has animation: Idle, Swim, and Attack

--- Under the Sea/Assets/Scripts/PlayerCamFollow.cs
+++ Under the Sea/Assets/Scripts/PlayerCamFollow.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCamFollow : MonoBehaviour {
+public class PlayerCamFollow : MonoBehaviour
+{
 
     private GameObject cam;         // To hold camera object
     private GameObject player;      // To hold player object
@@ -12,6 +13,8 @@ public class PlayerCamFollow : MonoBehaviour {
     private float z_playerOffset;   // To hold a value for z
     private float player_speed;     // To hold the speed of the player's movement
     private float camera_speed;     // To hold the speed of the camera mevement
+    private float left_rotation;    // To hold the left rotation of the player object
+    private float right_rotation;   // To hold the right rotation of the player object
 
     // Use this for initialization
     void Start()
@@ -20,8 +23,8 @@ public class PlayerCamFollow : MonoBehaviour {
         quadrant = 0;                                           // set quadrant to the first one
         cam = GameObject.FindGameObjectWithTag("MainCamera");   // set cam GameObject to the main camera object
         player = GameObject.FindGameObjectWithTag("Player");    // set player GameObject to the player object
-        camera_speed = 8f;                                      // set camera speed to 8, follow speed to get to player
-        player_speed = 0.1f;                                    // set player speed to 0.1f, contant speed of movement underwater of otter
+        camera_speed = 10f;                                     // set camera speed to 8, follow speed to get to player
+        player_speed = 0.05f;                                   // set player speed to 0.1f, contant speed of movement underwater of otter
     }
 
     void LateUpdate()
@@ -43,6 +46,7 @@ public class PlayerCamFollow : MonoBehaviour {
         // each quadrant that changes these things:
         // 1. setting directional movement
         // 2. Changing camera offset
+        // 3. setting rotation movement
         switch (quadrant)
         {
             // if -1, set to quadrant 3, looping
@@ -52,21 +56,29 @@ public class PlayerCamFollow : MonoBehaviour {
             case 0:
                 SetX(0.0f);
                 SetZ(-player_speed);
+                SetLeftRotation(0);
+                SetRightRotation(180);
                 cam_offset = new Vector3(player.transform.position.x - 8f, cam.transform.position.y, player.transform.position.z);
                 break;
             case 1:
                 SetX(player_speed);
                 SetZ(0.0f);
+                SetLeftRotation(270);
+                SetRightRotation(90);
                 cam_offset = new Vector3(player.transform.position.x, cam.transform.position.y, player.transform.position.z - 8f);
                 break;
             case 2:
                 SetX(0.0f);
                 SetZ(player_speed);
+                SetLeftRotation(180);
+                SetRightRotation(0);
                 cam_offset = new Vector3(player.transform.position.x + 8f, cam.transform.position.y, player.transform.position.z);
                 break;
             case 3:
                 SetX(-player_speed);
                 SetZ(0.0f);
+                SetLeftRotation(90);
+                SetRightRotation(270);
                 cam_offset = new Vector3(player.transform.position.x, cam.transform.position.y, player.transform.position.z + 8f);
                 break;
             // if 4, set to quadrant 1, looping
@@ -86,6 +98,30 @@ public class PlayerCamFollow : MonoBehaviour {
     public void MoveQuadrantLeft()
     {
         quadrant--;
+    }
+
+    // set left rotation, to said value
+    public void SetLeftRotation(float lr)
+    {
+        left_rotation = lr;
+    }
+
+    // get left rotation value
+    public float GetLeftRotation()
+    {
+        return left_rotation;
+    }
+
+    // set right rotation, to said value
+    public void SetRightRotation(float rr)
+    {
+        right_rotation = rr;
+    }
+
+    // get right rotation value
+    public float GetRightRotation()
+    {
+        return right_rotation;
     }
 
     // set players x offset, to said value
