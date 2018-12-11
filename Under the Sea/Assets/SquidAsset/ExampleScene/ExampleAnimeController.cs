0d5ab842bb0949aaa4679e6f1b761a6055b2a2b3
abe <adealba@csumb.edu>
2018-12-11 13:37:32 -0800

    squid crab assets

---
+++ Under the Sea/Assets/SquidAsset/ExampleScene/ExampleAnimeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExampleAnimeController : MonoBehaviour {
+
+    Animator anime;
+
+    void Awake()
+    {
+        anime = FindObjectOfType<Animator>();
+    }
+    void Start()
+    {
+        anime.SetTrigger("Idle");
+    }
+    public void PlayAnime(string _triggerID)
+    {
+        anime.SetTrigger(_triggerID);
+    }
+	
+}
